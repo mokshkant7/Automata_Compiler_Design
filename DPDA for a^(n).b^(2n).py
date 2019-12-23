36958d5627369bbb62ca54d57e9f72842faf45b7
Moksh Kant <42169641+mokshkant7@users.noreply.github.com>
2019-12-23 14:20:13 +0530

    Create DPDA for a^(n).b^(2n).py

---
+++ DPDA for a^(n).b^(2n).py
@@ -0,0 +1,32 @@
+#DPDA for a^(n).b^(2n)
+stack=['Z']
+state=0
+string=input('Enter input:')
+i=0
+flag=0
+len_string=len(string)
+for ch in string:
+	flag=0
+	print(ch)
+	if ch=='a' and state==0:
+		stack.append(ch)
+		flag=1
+	elif ch=='b' and stack[-1]=='a' and state==0:
+		stack.pop()
+		state=1
+		flag=1
+	elif (ch=='b' and stack[-1]=='a' and state==1) or(ch=='b' and stack[-1]=='Z' and state==1) :
+		state=0
+		flag=1
+	print('State:q'+str(state))
+	print('Stack:'+str(stack))
+	i=i+1
+# print('State:'+str(state)+'Stack:'+str(stack)+'i:'+str(i))
+if state==0 and len(stack)==1 and stack[-1]=='Z' and i==len_string and flag==1:
+	state=2
+
+print('End')
+if state==2:
+	print('Accepted')
+else:
+	print('Not Accepted')
